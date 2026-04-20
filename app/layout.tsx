b5935f647cef9a8698e58c35529971055d6a3ecd
Chandru <chandrukmc00@gmail.com>
2026-04-20 14:45:16 +0530

    Fixed TypeScript error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { Providers } from "./providers";
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&family=Montserrat:wght@500;700&display=swap" rel="stylesheet"></link>
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body>
